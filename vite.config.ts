51624c52fbd8d28fa28f78371a2293cd1f7dc900
Valentin <ardeleanvalentin490@yahoo.com>
2025-12-08 19:15:04 +0200

    Operator engines in UI (#52)
    * Rezolvare eroare rulare.
    
    * Add 'Engines' dropdown menu to sidebar

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@modules": path.resolve(__dirname, "src/modules"),
+alias: {
+  "@": path.resolve(__dirname, "src"),
+  "@modules": path.resolve(__dirname, "src/modules"),
       "@components": path.resolve(__dirname, "src/components"),
       "@core": path.resolve(__dirname, "src/core"),
       "@routes": path.resolve(__dirname, "src/routes"),
